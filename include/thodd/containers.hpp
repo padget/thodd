e726203fe65a1bf18b45006dd5d8bbb9787436cc
Benjamin MATHIEU <benjamin-externe.mathieu@erdf.fr>
2017-03-10 17:01:09 +0100

    reorg

--- include/thodd/containers.hpp
+++ include/thodd/containers.hpp
@@ -1,15 +1,15 @@
 #ifndef __THODD_CONTAINERS_HPP__
 #  define __THODD_CONTAINERS_HPP__
 
-#  include <thodd/container_iterator_traits.hpp>
-#  include <thodd/container_dsl.hpp>
-#  include <thodd/container_func.hpp>
+#  include <thodd/container/iterator_traits.hpp>
+#  include <thodd/container/dsl.hpp>
+#  include <thodd/container/func.hpp>
 
-#  include <thodd/container_range.hpp>
-#  include <thodd/container_list.hpp>
-#  include <thodd/container_set.hpp>
-#  include <thodd/container_map.hpp>
-#  include <thodd/container_vector.hpp>
-#  include <thodd/container_view.hpp>
+#  include <thodd/container/range.hpp>
+#  include <thodd/container/list.hpp>
+#  include <thodd/container/set.hpp>
+#  include <thodd/container/map.hpp>
+#  include <thodd/container/vector.hpp>
+#  include <thodd/container/view.hpp>
 
 #endif
